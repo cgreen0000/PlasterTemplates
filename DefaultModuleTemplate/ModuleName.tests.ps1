fa5adde60f59e8b6ac868694eaa88e9d813c24fe
cgreen0000 <green.chris@gmail.com>
2020-02-19 23:27:32 -0500

    More general tests.

--- DefaultModuleTemplate/ModuleName.tests.ps1
+++ DefaultModuleTemplate/ModuleName.tests.ps1
@@ -5,3 +5,49 @@ Describe "General module tests for: <%=$PLASTER_PARAM_ModuleName%>" {
         {Import-Module "$BuildRoot\<%=$PLASTER_PARAM_ModuleName%>\<%=$PLASTER_PARAM_ModuleName%>.psm1" -force } | Should Not Throw
     }
 }
+
+Describe "Public commands have Pester tests." {
+    $commands = Get-Command -Module "<%=$PLASTER_PARAM_ModuleName%>"
+
+    foreach ($command in $commands) {
+        $file = Get-ChildItem -Path "$BuildRoot\<%=$PLASTER_PARAM_ModuleName%>\Tests" -Include "$command.Tests.ps1" -Recurse
+
+        It "Should have a Pester test for [$command]" {
+            $file.FullName | Should -Not -BeNullOrEmpty
+        }
+    }
+}
+
+Describe "Public commands have comment-based or external help." {   
+    $functions = Get-Command -Module "<%=$PLASTER_PARAM_ModuleName%>"
+
+    $help = foreach ($function in $functions) {
+        Get-Help -Name $function.Name
+    }
+
+    foreach ($node in $help) {
+        Context $node.Name {
+            It "Should have a description." {
+                $node.Description | Should -Not -BeNullOrEmpty
+            }
+            It "Should have an example." {
+                $node.Examples | Should -Not -BeNullOrEmpty
+            }
+
+            foreach ($helpExample in $node.Examples.Example) {
+                $title = $helpExample.Title
+                It "$title should include the function being demonstrated." {
+                    $helpExample | Out-String | Should -Match ($node.Name)
+                }
+            }
+
+            foreach ($parameter in $node.Parameters.Parameter) {
+                if ($parameter -notmatch 'WhatIf|Confirm') {
+                    It "Should have a description for Parameter [$($parameter.Name)]" {
+                        $parameter.Description.Text | Should -Not -BeNullOrEmpty
+                    }
+                }
+            }
+        }
+    }
+}
